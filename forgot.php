74a740c00af8a9753c759707b421275c882b8851
gaurav <gauravkumar5823@gmail.com>
2019-08-10 01:19:17 +1200

    Create forgot.php
    create new page for email check for password reset

---
+++ forgot.php
@@ -0,0 +1,37 @@
+<?php
+ require 'server.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>forgot password</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+  <div class="header">
+    <h2>Password Reset</h2>
+  </div>
+  <form method="post" action="forgot.php">
+
+
+    <div class="input-group">
+      <label>Email</label>
+      <input type="email" name="email" required>
+    </div>
+
+<div class="input-group">
+  <button type="submit" name="forgot" class="btn">reset</button>
+</div>
+
+<p>  go to login page  <a href="login.php">login </a>
+</p>
+
+
+  </form>
+
+</body>
+
+
+
+</html>
