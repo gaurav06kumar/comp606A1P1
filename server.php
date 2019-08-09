582d56b58a94f08ba50e6777617487a08ed6a36f
gaurav <gauravkumar5823@gmail.com>
2019-08-10 01:20:09 +1200

    add new isset function
    add forget password function

--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+   session_start();
 $mysql_host='localhost';
 $mysql_user='root';
 $mysql_password='';
@@ -9,43 +9,64 @@ $db=mysqli_connect($mysql_host,$mysql_user,$mysql_password,$database);
 
 if($db->connect_error)
 {
-  die("Connection failed: " . $db->connect_error);
+ die("Connection failed: " . $db->connect_error);
 }
 else{
 
-
 }
 
 
- if(isset($_POST['register']))
+if(isset($_POST['register']))
 {
-    $username=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
+ $username=$_POST['username'];
+ $email=$_POST['email'];
+ $password =$_POST['password_1'];
+ $password2 = $_POST['password_2'];
 
 
+ if($password == $password2)
+ {
+
+   $check_query = "SELECT * FROM user WHERE username='$username' OR email='$email' ";
+   $result = mysqli_query($db, $check_query);
+   $user = mysqli_fetch_assoc($result);
+
+   if (mysqli_num_rows($result) > 0)
+   {
+     echo '<script language="javascript">';
+     echo 'alert("Username and email alredy exits")';
+     echo '</script>';
 
 
-    $sql="INSERT INTO user (username,  email,password) VALUES ('$username','$email','$password')";
-
-
-    $result=$db->query($sql);
-
-
-    if( $result== TRUE)
+   }
+   else
     {
-
-        echo ' run ';
-        $_SESSION['username']=$username;
-        $_SESSION['sucess']=" You are now logged in";
-        header('location:profile.php');
+     $sql="INSERT INTO user (username,  email,password) VALUES ('$username','$email','$password')";
 
 
-    }
-    else
-    {
-        echo 'not run';
-    }
+     $result=mysqli_query($db, $sql);
+
+
+     if( $result== TRUE)
+          {
+       $_SESSION['username']=$username;
+       $_SESSION['sucess']=" You are now logged in";
+       header('location:profile.php');
+           }
+
+   }
+
+}
+
+   else
+   {
+
+
+       echo '<script language="javascript">';
+       echo 'alert("wrong password")';
+       echo '</script>';
+
+   }
 
 }
 
@@ -55,57 +76,30 @@ else{
 if(isset($_POST['login']))
 {
 
-  $username=$_POST['username'];
-  $password =$_POST['password'];
-
-
-  //$sql="select * from user  ";
-  //$sql="SELECT * FROM user  WHERE email='$email' ";
-
-  $sql = "SELECT * FROM user WHERE username='$username'and password='$password' ";
-
-
-  $result = mysqli_query($db,$sql) or die(mysql_error());
-  $rows = mysqli_num_rows($result);
-  if( $result== TRUE)
-  {
-
-      echo ' run<br>';
-
-      while($row = $result->fetch_assoc()){
-          echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["email"]." ".$row["password"]."<br>";
-
-        }
+ $username=$_POST['username'];
+ $password =$_POST['password'];
 
 
 
+ $sql = "SELECT * FROM user WHERE username='$username'and password='$password' ";
 
 
-  }
-  else
-  {
-      echo 'error';
-  }
+ $result = mysqli_query($db,$sql) or die(mysql_error());
+ $rows = mysqli_num_rows($result);
 
 
+ if($rows==1)
+ {
+   $_SESSION['username']=$username;
+   $_SESSION['sucess']="you are now logged in";
+   header('location:profile.php');
+ }
 
-
-
-
-
-  if($rows==1)
-  {
-
-    echo "running";
-
-    $_SESSION['username']=$username;
-    $_SESSION['sucess']="you are now logged in";
-    header('location:profile.php');
-   }
-
-   else{
-       echo "not running ";
-   }
+ else{
+   echo '<script language="javascript">';
+   echo 'alert("Username/password is incorrect")';
+   echo '</script>';
+ }
 
 
 
@@ -114,11 +108,71 @@ if(isset($_POST['login']))
 }
 
 
+if(isset($_POST['forgot']))
+{
+
+  $email=$_POST['email'];
+  $sql="SELECT * FROM user WHERE email='$email'   ";
+  $result=mysqli_query($db,$sql) or die(mysql_error());
+  $row=mysqli_num_rows($result);
+  if($row==1)
+  {
+
+     header('location:resetpass.php');
+        $_SESSION['email']=$_POST['email'];
+  }
+  else {
+    echo '<script language="javascript">';
+    echo 'alert("email is not correct")';
+    echo '</script>';
+
+  }
+
+
+}
+
+if(isset($_POST['change']))
+{
+
+   $email=$_SESSION['email'];
+   $password =$_POST['password_1'];
+   $password2 = $_POST['password_2'];
+
+if($password==$password2)
+{
+
+   $sql = "UPDATE user SET password='$password' WHERE email='$email'";
+$result=mysqli_query($db, $sql);
+if ($result=TRUE) {
+  echo '<script language="javascript">';
+  echo 'alert("password changed")';
+  echo '</script>';
+     header('location:login.php');
+} else {
+  echo '<script language="javascript">';
+  echo 'alert("password not changed")';
+  echo '</script>';
+}
+}
+else {
+  {
+
+
+
+        echo '<script language="javascript">';
+        echo 'alert("  password not matched")';
+        echo '</script>';
+
+
+  }
+}
 
 
 
 
 
+
+}
 
 
 
